e79e18005ce45d1cc3d929d34d549805a13a081c
gebrilae-eng <gebrilae@gmail.com>
2026-03-17 21:06:53 +0200

    feat: add install.bat one-click installer

---
+++ install.bat
@@ -0,0 +1,20 @@
+@echo off
+echo.
+echo  ag_ai - Update and Install
+echo  ===========================
+echo.
+
+cd /d C:\temp\ag_ai
+echo [1/3] Pulling latest from GitHub...
+git pull
+echo.
+
+echo [2/3] Running setup...
+if "%1"=="" (
+    python setup_ai.py
+) else (
+    python setup_ai.py %1
+)
+echo.
+echo [3/3] Done!
+pause
