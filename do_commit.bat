8d8b7486e28653a8bfaf92adbd78baf8b3a8fc3c
gebrilae-eng <gebrilae@gmail.com>
2026-03-18 13:02:13 +0200

    cleanup temp file

--- do_commit.bat
+++
@@ -1,5 +0,0 @@
-cd C:\ag_ai
-git rm --cached do_commit.bat 2>nul
-git add -A
-git commit -m "chore: remove do_commit.bat temp file"
-git push origin main
---
+++ x.bat
@@ -0,0 +1,5 @@
+cd C:\ag_ai
+git rm --force do_commit.bat
+git add -A
+git commit -m "cleanup temp file"
+git push origin main
